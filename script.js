d1f4dd600818afb6d9e1110d101c823eb70c529e
favolia <daisszo1455@gmail.com>
2025-05-31 22:04:46 +0800

    fix: disable scroll on loading

--- script.js
+++ script.js
@@ -1,8 +1,55 @@
+const ScrollLocker = (() => {
+    let scrollX = 0;
+    let scrollY = 0;
+
+    function preventDefault(e) {
+        e.preventDefault();
+    }
+
+    function lockScroll() {
+        scrollX = window.scrollX || window.pageXOffset;
+        scrollY = window.scrollY || window.pageYOffset;
+
+        window.scrollTo(scrollX, scrollY);
+        window.addEventListener('scroll', freezeScroll, { passive: false });
+        window.addEventListener('wheel', preventDefault, { passive: false });
+        window.addEventListener('touchmove', preventDefault, { passive: false });
+        window.addEventListener('keydown', preventKeyScroll, false);
+    }
+
+    function unlockScroll() {
+        window.removeEventListener('scroll', freezeScroll);
+        window.removeEventListener('wheel', preventDefault);
+        window.removeEventListener('touchmove', preventDefault);
+        window.removeEventListener('keydown', preventKeyScroll);
+    }
+
+    function freezeScroll() {
+        window.scrollTo(scrollX, scrollY);
+    }
+
+    function preventKeyScroll(e) {
+        const keys = [32, 33, 34, 35, 36, 37, 38, 39, 40];
+        if (keys.includes(e.keyCode)) {
+            e.preventDefault();
+        }
+    }
+
+    return {
+        lock: lockScroll,
+        unlock: unlockScroll
+    };
+})();
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     window.scrollTo({
         top: 0,
-        behavior: 'smooth' // scroll halus
+        behavior: 'smooth'
     });
+
+    ScrollLocker.lock();
 
     const loadingScreen = document.querySelector('#loading-screen')
     const loadingText = document.querySelector('#loading-text')
@@ -31,10 +78,11 @@ document.addEventListener('DOMContentLoaded', () => {
     animateLetterHighlight();
 
     setTimeout(() => {
-        document.body.classList.remove('overflow-hidden')
+        document.querySelector('html').classList.remove('overflow-hidden')
         loadingScreen.classList.add('opacity-0')
         setTimeout(() => {
             loadingScreen.remove()
+            ScrollLocker.unlock();
         }, 500);
     }, 3000);
 
@@ -63,9 +111,9 @@ document.addEventListener('DOMContentLoaded', () => {
         htmlToImage.toPng(scheduleImage)
             .then(function (dataUrl) {
                 const link = document.createElement('a');
-                link.download = 'schedule.png'; // nama file
+                link.download = 'schedule.png';
                 link.href = dataUrl;
-                link.click(); // trigger download
+                link.click();
             })
             .catch(function (error) {
                 console.error('oops, something went wrong!', error);
